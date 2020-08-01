df28192140a1c5e096d8166a8db5ab5f50bd3eb3
fabioportella2009@gmail.com <FabioPortella>
2020-08-01 13:13:20 -0300

    acrescentando tratamento de erros

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template, request, escape, session
+
 from vsearch import procura_letras
+
 from checker import check_logged_in
-from DBcm import UseDatabase
+from DBcm import UseDatabase, ConnectionError, CredentialsError, SQLError
+
+from time import sleep
 
 app = Flask(__name__)
+
 app.secret_key = "MeuSenhorJesusCristo"
 
 app.config['dbconfig'] = {'host': '127.0.0.1',
@@ -23,19 +28,29 @@ def do_logout() -> str:
     session.pop('logged_in')
     return "Você NÃO está mais logado"
 
-     
 def log_request(req: 'flask_request',  res: str) -> None:
     """Log detils of the web reueste and the reultes. """
-    with UseDatabase(app.config['dbconfig']) as cursor:
-        _SQL = """insert into log
-                (phrase, letters, ip, browser_string, results)
-                values
-                (%s, %s, %s, %s, %s)"""
-        cursor.execute(_SQL, (req.form['phrase'],
-                              req.form['letters'],
-                              req.remote_addr,
-                              req.user_agent.browser,
-                              res, ))
+    try:
+        with UseDatabase(app.config['dbconfig']) as cursor:
+            _SQL = """insert into log
+                    (phrase, letters, ip, browser_string, results)
+                    values
+                    (%s, %s, %s, %s, %s)"""
+            cursor.execute(_SQL, (req.form['phrase'],
+                                req.form['letters'],
+                                req.remote_addr,
+                                req.user_agent.browser,
+                                res, ))
+    except ConnectionError as err:
+        print("O seu Banco de Dados está ligado? Erro: ", str(err))
+    except CredentialsError as err:
+        print("Erro de Usuário/Senha. Error: ", str(err))
+    except SQLError as err:
+        print("Sua QUERY está correta?  Error: ", str(err))    
+    except Exception as err:
+        print("Algo deu errado: ", str(err))
+    return "Error"     
+
 
 
 @app.route('/search4', methods=['POST'])
@@ -44,7 +59,10 @@ def do_search() -> 'html':
     letras = request.form['letters']
     title = "Estes são os seus resultados:"
     results = str(procura_letras(frase, letras))
-    log_request(request, results)
+    try:
+        log_request(request, results)
+    except  Exception as err:
+        print('***** Logging failed with this error', str(err))
     return render_template('results.html',
         the_phrase=frase,
         the_letters=letras,
@@ -55,23 +73,34 @@ def do_search() -> 'html':
 @app.route('/')
 @app.route('/entrada')
 def entry_page() -> 'html':
-    return render_template('entry.html', the_title="Bem vindo ao search4leters para WEB")
+    return render_template('entry.html', 
+                            the_title="Bem vindo ao search4leters para WEB")
 
 
 @app.route('/verlog')
 @check_logged_in
 def view_the_log() -> 'html':
     """Display the contentes of the log file as a HTML table."""
-    with UseDatabase(app.config['dbconfig']) as cursor:
-        _SQL = """select phrase, letters, ip, browser_string, results
-                  from log"""
-        cursor.execute(_SQL)
-        contents = cursor.fetchall()
-    titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
-    return render_template('viewlog.html',
-                           the_title='View Log',
-                           the_row_title=titles,
-                           the_data=contents,)
+    try:
+        with UseDatabase(app.config['dbconfig']) as cursor:
+            _SQL = """select phrase, letters, ip, browser_string, results
+                      from log"""
+            cursor.execute(_SQL)
+            contents = cursor.fetchall()
+        titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
+        return render_template('viewlog.html',
+                               the_title='View Log',
+                               the_row_title=titles,
+                                the_data=contents,)
+    except ConnectionError as err:
+        print("O seu Banco de Dados está ligado? Erro: ", str(err))
+    except CredentialsError as err:
+        print("Erro de Usuário/Senha. Error: ", str(err))
+    except SQLError as err:
+        print("Sua QUERY está correta?  Error: ", str(err))    
+    except Exception as err:
+        print("Algo deu errado: ", str(err))
+    return "Error"
 
 
 if __name__ == "__main__":
